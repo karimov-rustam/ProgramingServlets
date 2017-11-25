96d65e8e9a7592dbeebdc706d553ed0e7b0fa8f9
Karimov R <rkarimov.r@gmail.com>
2017-11-25 22:56:44 +0400

    Handling form data in servlet

---
+++ src/main/java/edu/web/Register.java
@@ -0,0 +1,55 @@
+package edu.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by R.Karimov on 11/25/17.
+ */
+@WebServlet(name = "Register324", urlPatterns = "/Register")
+public class Register  extends HttpServlet {
+
+    public Register() {
+//        super();
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String username = request.getParameter("userName");
+        String passwd = request.getParameter("passwd");
+        String gender = request.getParameter("gender");
+        String[] hobbies = request.getParameterValues("hobbies");
+        String country = request.getParameter("country");
+        String[] languages = request.getParameterValues("languages");
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<div>");
+        out.println("<p>User name: " + username + "</p>");
+        out.println("<p>Password: " + passwd + "</p>");
+        out.println("<p>Sex: " + gender + "</p>");
+        out.println("<p>Hobbies: </p>");
+
+        for (String hobby : hobbies) {
+            out.println(hobby + "<br>");
+        }
+
+        out.println("<p>Country: " + country + "</p>");
+
+        out.println("<p>Languages known: </p>");
+        for (String lang : languages) {
+            out.println(lang + "<br>");
+        }
+
+        }
+    }
